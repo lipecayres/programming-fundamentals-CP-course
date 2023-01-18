1f61657583b9c7e8e6c8e981849596b0b4ef2e7e
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-18 09:29:07 -0500

    first boolean variable

--- MyFirstProject/src/JavaCodeTester1.java
+++ MyFirstProject/src/JavaCodeTester1.java
@@ -1,11 +1,16 @@
-/**	Application Purpose:
-*	Author:
-*	Date:
-*	Time:
+/**	Application Purpose: Become familiar with basic operations on Java language  
+*	Author: Felipe Cayres
+*	Date: Jan 18th, 2023
+*	Time: 08:50
 */
 
 public class JavaCodeTester1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        // Creating first boolean variable
+        boolean booleanVariableTest = false; 
+        System.out.println(booleanVariableTest);
+
+
     }
 }
