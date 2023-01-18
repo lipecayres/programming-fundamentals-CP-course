d4c1904b5f7dce557ade8ea01e677e91c1afac3b
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-18 08:47:10 -0500

    starting point

--- MyFirstProject/src/JavaCodeTester1.java
+++ MyFirstProject/src/JavaCodeTester1.java
@@ -1,4 +1,10 @@
-public class App {
+/**	Application Purpose:
+*	Author:
+*	Date:
+*	Time:
+*/
+
+public class JavaCodeTester1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
