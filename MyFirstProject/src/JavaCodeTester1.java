ae7714647c43262b3c84d967a9e3d6931d0c9d40
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-18 09:37:06 -0500

    add comments - activity steps

--- MyFirstProject/src/JavaCodeTester1.java
+++ MyFirstProject/src/JavaCodeTester1.java
@@ -4,29 +4,29 @@
 *	Time: 13:00
 */
 
-public class JavaCodeTester1 {
-    public static void main(String[] args) {
+public class JavaCodeTester1 {                          // Step 1
+    public static void main(String[] args) {            //Step 2 and 3
         
-        // Creating first boolean variable
-        boolean booleanVariableTest = false; 
-        System.out.println(booleanVariableTest);
+        // Creating first boolean variable  
+        boolean booleanVariableTest = false;            //Step 4
+        System.out.println(booleanVariableTest);        //Step 5
 
         // Creating first char variable        
-        char charVariableTest = '#';
-        System.out.println(charVariableTest);
+        char charVariableTest = '#';                    //Step 6
+        System.out.println(charVariableTest);           //Step 7
 
         // Concatenating strings variables at Java              
-        String stringTest1 = "String1"; 
-        String stringTest2 = "String2";
-        System.out.println(stringTest1 + stringTest2);
+        String stringTest1 = "String1";                 //Step 8
+        String stringTest2 = "String2";                 //Step 8
+        System.out.println(stringTest1 + stringTest2);  //Step 9
 
         // Creating first integer variable              
-        int integerTest = 0;
-        System.out.println(integerTest);
+        int integerTest = 0;                            //Step 10
+        System.out.println(integerTest);                //Step 11
 
         // Creating first char variable              
-        double doubleTest = 0.0;
-        System.out.println(doubleTest);
+        double doubleTest = 0.0;                        //Step 12
+        System.out.println(doubleTest);                 //Step 13
 
 
     }
