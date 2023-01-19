ae63d6bdabf303c82d8a84d7d870d34c78d385eb
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-18 23:57:12 -0500

    step 11 - increment after

--- MyFirstProject/src/JavaCodeTester2.java
+++ MyFirstProject/src/JavaCodeTester2.java
@@ -26,6 +26,11 @@ public class JavaCodeTester2 {                  // Step 1 - declaration called J
         System.out.println(++integerTest1);         // Step 10 - Use the increment operator to display the value of the variable 
         System.out.println(integerTest1);           //      (Increment before)
 
+        int integerTest2 = 0;                       //  
+        System.out.println(integerTest1);           // Step 11 - Use the increment operator to display the value of the same variable
+        System.out.println(integerTest1++);         //      (increment after)
+        System.out.println(integerTest1);           //
+
 
 
         }
