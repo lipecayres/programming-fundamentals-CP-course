7097571ab669455ba6cbdc394770470f14c26aa5
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-18 23:34:15 -0500

    javadoc file created

--- MyFirstProject/src/JavaCodeTester2.java
+++ MyFirstProject/src/JavaCodeTester2.java
@@ -1,11 +1,10 @@
-/**	Application Purpose:
-*	Author:
-*	Date:
-*	Time:
+/**	Application Purpose: Learn methods and bacis operations on Java
+*	Author: Felipe Cayres
+*	Date: Jan 18th, 2023
+*	Time: 11 PM
 */
 
 public class JavaCodeTester2 {
     public static void main(String[]args) {
-        
-    }
+        }
 }
