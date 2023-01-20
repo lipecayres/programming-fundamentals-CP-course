8f44bcab58572ee551654191e52387336be4a6fa
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-20 00:25:54 -0500

    renamed variable names (meaningfull examples)

--- week2/src/JavaCodeTester1.java
+++ week2/src/JavaCodeTester1.java
@@ -1,33 +1,36 @@
-/**	Application Purpose: Challenge 1 - Become familiar with basic operations on Java  
+/**	Application Purpose: Become familiar with basic operations on Java  
 *	Author: Felipe Cayres
 *	Date: Jan 18th, 2023
 *	Time: 13:00
 */
 
+    //
+    // Challenge 1 
+    //
+    
 public class JavaCodeTester1 {                          // Step 1
     public static void main(String[] args) {            //Step 2 and 3
         
         // Creating first boolean variable  
-        boolean booleanVariableTest = false;            //Step 4
-        System.out.println(booleanVariableTest);        //Step 5
+        boolean happyDay = true;            //Step 4
+        System.out.println(happyDay);        //Step 5
 
         // Creating first char variable        
-        char charVariableTest = '#';                    //Step 6
-        System.out.println(charVariableTest);           //Step 7
+        char atSignal = '@';                    //Step 6
+        System.out.println(atSignal);           //Step 7
 
         // Concatenating strings variables at Java              
-        String stringTest1 = "String1";                 //Step 8
-        String stringTest2 = "String2";                 //Step 8
-        System.out.println(stringTest1 + stringTest2);  //Step 9
+        String firstName = "Felipe";                 //Step 8
+        String lastName = "Cayres";                 //Step 8
+        System.out.println(firstName + lastName);  //Step 9
 
         // Creating first integer variable              
-        int integerTest = 0;                            //Step 10
-        System.out.println(integerTest);                //Step 11
+        int age = 29;                            //Step 10
+        System.out.println(age);                //Step 11
 
         // Creating first char variable              
-        double doubleTest = 0.0;                        //Step 12
-        System.out.println(doubleTest);                 //Step 13
-
+        double speed = 13.65;                        //Step 12
+        System.out.println(speed);                 //Step 13
 
     }
 }
--- week2/src/JavaCodeTester2.java
+++ week2/src/JavaCodeTester2.java
@@ -1,4 +1,4 @@
-/**	Application Purpose: Learn methods and bacis operations on Java
+/**	Application Purpose: Learn methods and basic operations on Java
 *	Author: Felipe Cayres
 *	Date: Jan 18th, 2023
 *	Time: 11 PM
@@ -6,51 +6,53 @@
 
     // Challenge 2
 
-public class JavaCodeTester2 {                  // Step 1 - declaration called JavaCodeTester1  
-    public static void main(String[]args) {     // Step 2 and 3 - main method
-        String stringTest1 = "String1";         //    
-        String stringTest2 = "String2";         // Step 4 - Store three String values in three different variables 
-        String stringTest3 = "String3";         //
+public class JavaCodeTester2 {                  // Step 1
+    public static void main(String[]args) {     // Step 2 and 3
+        String firstName = "Felipe";          //    
+        String secondName = "Simoes";         // Step 4 - 
+        String lastName = "Cayres";           //
 
-        String stringsConcatenate = stringTest1 + stringTest2 + stringTest3;   // Step 5 - Concatenate the three Strings 
+        String fullName = firstName + secondName + lastName;   // Step 5
 
-        System.out.println(stringsConcatenate.charAt(2) + " " + stringsConcatenate.charAt(4) + " " + stringsConcatenate.charAt(6));
-        System.out.println("These are the 3rd, 5th and 7th character of the concatenated string!");   // Step 6 - Display the 3rd and 5th and 7th character + message.
+        System.out.println(fullName.charAt(2) + " " + fullName.charAt(4) + " " + fullName.charAt(6));
+        System.out.println("These are the 3rd, 5th and 7th character of the concatenated string!");   // Step 6 
 
-        System.out.println(stringsConcatenate.startsWith("test"));      // Step 7 - Display the Boolean value (start with "test"?)
+        System.out.println(fullName.startsWith("test"));      // Step 7 
 
-        System.out.println(stringsConcatenate.toUpperCase());       // Step 8 - display string in all upper case letters
+        System.out.println(fullName.toUpperCase());       // Step 8 
 
-        int integerTest1 = 0;               // Step 9 - Store an integer value in a variable
+        int incrementBeforeTest = 0;               // Step 9
 
-        System.out.println(integerTest1);           //
-        System.out.println(++integerTest1);         // Step 10 - Use the increment operator to display the value of the variable 
-        System.out.println(integerTest1);           //      (Increment before)
+        System.out.println(incrementBeforeTest);           //
+        System.out.println(++incrementBeforeTest);         // Step 10 
+        System.out.println(incrementBeforeTest);           //     
 
-        int integerTest2 = 0;                       //  
-        System.out.println(integerTest2);           // Step 11 - Use the increment operator to display the value of the same variable
-        System.out.println(integerTest2++);         //      (increment after)
-        System.out.println(integerTest2);           //
+        int incrementAfterText = 0;                       //  
+        System.out.println(incrementAfterText);           // Step 11 
+        System.out.println(incrementAfterText++);         //     
+        System.out.println(incrementAfterText);           //
 
         // Question - What did you notice about the print outs?
 
-        /* When we place the increment operator before the variable, it's gonna add a number before showing at the display.
+        /* When we place the increment operator before the variable, it's going to add a number before showing at the display.
          * This way we are available to see the incremented number at first following occurrence.
          *  
-         * But when we place the increment operator after the variable, the sum is gonna happen after the number showing
+         * But when we place the increment operator after the variable, the sum is going to happen after the number showing
          * at the display. So, we just are available to see the incremented number at the second following occurrence. 
         */
-
+    
+    //
     // Challenge 3
+    //
 
-    int numberTest = 15;        // Step 1 - store an integer value in a variable 
+        int eggsPack = 12;        // Step 1 
 
-    if (numberTest % 2 == 0){           // Step 2 - modulus operator to determine if is even or odd
-        System.out.println("The number is even");
-    } else {
-        System.out.println("The number is odd");
-    }
-
+        if (eggsPack % 2 == 0){           // Step 2
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
         }
+
+    }
 }
     
