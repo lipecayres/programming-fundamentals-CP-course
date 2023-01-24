35a7e9eed3e44b084519138336eb36f1b0ac5164
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-24 14:16:51 -0500

    challenge 3 - setting new first and last name to account

--- week3/assignment3/src/ChequingAccountTestHarness.java
+++ week3/assignment3/src/ChequingAccountTestHarness.java
@@ -5,11 +5,40 @@ public class ChequingAccountTestHarness {
 
     ChequingAccount account1 = new ChequingAccount("Joe", "Smith", 300.15 , 102030);
     
-    System.out.println("First Name: " + account1.getFirstName());
-    System.out.println("Last Name: " + account1.getLastName());
-    System.out.println("Balance: $" + account1.getBalance());
-    System.out.println("Account Number: " + account1.getAccountNumber());
+    System.out.println("Account Informations:");
+    System.out.println("--- " + "First Name: " + account1.getFirstName());
+    System.out.println("--- " +"Last Name: " + account1.getLastName());
+    System.out.println("--- " +"Balance: $" + account1.getBalance());
+    System.out.println("--- " +"Account Number: " + account1.getAccountNumber());
     
+    System.out.println();
 
-    }
+        // Challenge 3
+    
+    System.out.println("Account Informations Updated:");
+    account1.setFirstName("Camila");
+    account1.setLastName("Cayres");
+
+    System.out.println();
+    System.out.println("New First Name: " + account1.getFirstName());
+    System.out.println("New Last Name: " + account1.getLastName());
+    System.out.println();
+
+    //    System.out.println("Balance: $" + account1.getBalance());
+//    System.out.println("Account Number: " + account1.getAccountNumber());
+
+
+    System.out.println("Account Informations:");
+    System.out.println("--- " + "First Name: " + account1.getFirstName());
+    System.out.println("--- " +"Last Name: " + account1.getLastName());
+    System.out.println("--- " +"Balance: $" + account1.getBalance());
+    System.out.println("--- " +"Account Number: " + account1.getAccountNumber());
+
+System.out.println();
+
+
+
+    System.out.println();
+
+}
 }
