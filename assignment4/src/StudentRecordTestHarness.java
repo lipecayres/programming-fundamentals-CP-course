44564bf83be2c0ea6c48850934b45da653dd0e6d
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-01 10:18:47 -0500

    reshaping code - clean visual and add coments steb by step

--- assignment4/src/StudentRecordTestHarness.java
+++ assignment4/src/StudentRecordTestHarness.java
@@ -7,68 +7,66 @@
 // Import statements:
 import java.util.Scanner;
     
-public class StudentRecordTestHarness {                                                              // Class declaration
-    public static void main (String [] args) {                                                      //Main method
+public class StudentRecordTestHarness {                                                         // Step 1            Create a second class called StudentRecordTestHarness 
+    public static void main (String [] args) {                                                  // Main method
         
                     //
                     // Challenge 2
                     //
 
-        // Declaring the first Student Record (sr) 
+        // Step 2             Instantiate a StudentRecord object 
         StudentRecord sr1 = new StudentRecord("Joe", "Smith", 86 , 202301001);
 
-        // Printing on screen account informations
-        System.out.println();                                                                      // Jumping line - better visualization
-        System.out.println("Student Record Informations Example:");                                     
-        System.out.println("--- " + "First Name: " + sr1.getFirstName());
-        System.out.println("--- " +"Last Name: " + sr1.getLastName());
-        System.out.println("--- " +"Student ID: " + sr1.getStudentID());
-        System.out.println("--- " +"Grade Course One: " + sr1.getgradeCourseOne());
-        System.out.println("--- " +"Letter Grade: " + sr1.getLetterGrade());
+        // Step 3             Print out the data, including the letter grade, from this first record.
+        System.out.println();                                                                   // Jumping line - better visualization
+        System.out.println("Student Record Informations Example:");                           // Print "example" title to user        
+        System.out.println("--- " + "First Name: " + sr1.getFirstName());                       // Print First Name     
+        System.out.println("--- " +"Last Name: " + sr1.getLastName());                          // Print Last Name
+        System.out.println("--- " +"Student ID: " + sr1.getStudentID());                        // Print Student ID
+        System.out.println("--- " +"Grade Course One: " + sr1.getgradeCourseOne());             // Print Grade Letter One
+        System.out.println("--- " +"Letter Grade: " + sr1.getLetterGrade());                    // Print Letter Grade
         
-        System.out.println();                                                                      // Jumping line - better visualization
+        System.out.println("\n----------------------------");                                // Jumping line  and printing a bar to better visualization
 
-        System.out.println("----------------------------"); 
 
-        //      Step 4 - Instantiate a second student record with no data.
-        StudentRecord sr2 = new StudentRecord();
+        // Step 4 - Instantiate a second student record with no data.
+        StudentRecord sr2 = new StudentRecord();                
 
-        System.out.print("Registration: \n" + 
-                         "Type here the informations about the new student\n\n");
-
-        Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+        // Step 5 - Ask the user for the data required to fill each field of the record
+        System.out.print("Registration: \n");                                                 // Display appropriate message to user
+        System.out.print("Type here the informations about the new student\n\n")
+        
+        Scanner sc= new Scanner(System.in);                                                     // Set new Scanner to input data to system.  
         
                 // Register first name
-        System.out.print("Enter First Name: ");  
-        String firstName= sc.nextLine();              //reads string   
-        sr2.setFirstName(firstName);         
+        System.out.print("Enter First Name: ");                                               // Display appropriate messa to user
+        String firstName= sc.nextLine();                                                        // Input to First Name   
+        sr2.setFirstName(firstName);                                                            // Set method to save typed first name
 
                 // Register last name
-        System.out.print("Enter Last Name: ");  
-        String lastName= sc.nextLine();              //reads string   
-        sr2.setLastName(lastName);         
+        System.out.print("Enter Last Name: ");                                                // Display appropriate messa to user
+        String lastName= sc.nextLine();                                                         // Input to last Name
+        sr2.setLastName(lastName);                                                              // Set method to save typed last name
 
                 // Register Grades
-        System.out.print("Enter Grade: ");  
-        double gradeCourseOne= sc.nextDouble();              //reads string   
-        sr2.setgradeCourseOne(gradeCourseOne);  
+        System.out.print("Enter Grade: ");                                                    // Display appropriate messa to user
+        double gradeCourseOne= sc.nextDouble();                                                 // Input to grade course one
+        sr2.setgradeCourseOne(gradeCourseOne);                                                  // Set method to save typed grade course one
 
                 // Register Student ID                
-        System.out.print("Enter Student ID: ");  
-        int studentID= sc.nextInt();              //reads string   
-        sr2.setStudentID(studentID);  
+        System.out.print("Enter Student ID: ");                                               // Display appropriate messa to user
+        int studentID= sc.nextInt();                                                            // Input to student ID
+        sr2.setStudentID(studentID);                                                            // Set method to save typed first name
 
-        System.out.println();                                                                      // Jumping line - better visualization
-        System.out.println("Student Record Informations:");
-        System.out.println("--- " + "First Name: " + sr2.getFirstName());
-        System.out.println("--- " +"Last Name: " + sr2.getLastName());
-        System.out.println("--- " +"Student ID: " + sr2.getStudentID());
-        System.out.println("--- " +"Grade Course One: " + sr2.getgradeCourseOne());
-        System.out.println("--- " +"Letter Grade: " + sr2.getLetterGrade());
+        System.out.println();                                                                   // Jumping line - better visualization
+        System.out.println("Student Record Informations:");                                   // Print title to user - inputed data
+        System.out.println("--- " + "First Name: " + sr2.getFirstName());                       // Print first Name
+        System.out.println("--- " +"Last Name: " + sr2.getLastName());                          // Print last Name
+        System.out.println("--- " +"Student ID: " + sr2.getStudentID());                        // Print Student ID
+        System.out.println("--- " +"Grade Course One: " + sr2.getgradeCourseOne());             // Print Grade course ONE
+        System.out.println("--- " +"Letter Grade: " + sr2.getLetterGrade());                    // Print Letter Grade
         
-        System.out.println();                                                                      // Jumping line - better visualization
-
-        System.out.println("----------------------------"); 
+        System.out.println("\n----------------------------");                                // Jumping line  and printing a bar to better visualization
 
         System.out.println("--- " +"Vowels: " + sr2.getVowelCount());
         
