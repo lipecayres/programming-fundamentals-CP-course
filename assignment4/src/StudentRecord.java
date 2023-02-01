13bbe3f2b93e99567652b4fa4947f86d0804e424
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-01 09:54:47 -0500

    comments steb by step methods

--- assignment4/src/StudentRecord.java
+++ assignment4/src/StudentRecord.java
@@ -4,19 +4,20 @@
 *	Time: 8:27 PM
 */
 
-   public class StudentRecord  { 
+    // Step 1      Create a class called StudentRecord 
+public class StudentRecord  {                       
 
-    // Variables
+        // Variables
 
-private String firstName;
-private String lastName;
-private double gradeCourseOne;
-private int studentID;
+    private String firstName;
+    private String lastName;
+    private double gradeCourseOne;
+    private int studentID;
 
-    // Declaring empty constructor
+        // Declaring empty constructor
     public StudentRecord  () {}
 
-    // Constructor declaration 
+        // Constructor declaration with arguments 
     public StudentRecord  (String firstName, String lastName, double gradeCourseOne, int studentID) {
         this.firstName = firstName;
         this.lastName = lastName;           
@@ -24,81 +25,83 @@ private int studentID;
         this.studentID = studentID;
     }
 
- // Getters and Setters -> First Name
+        // Getters and Setters -> First Name
 
-public void setFirstName(String firstName) {
-    this.firstName = firstName;
-}
-
-public String getFirstName(){
-    return firstName;
-}
-
-    // Getters and Setters -> Last Name
-
-public void setLastName(String lastName) {
-    this.lastName = lastName;
-}
-
-public String getLastName() {
-    return lastName;
-}
-
-    // Getters and Setters -> Grade Course One
-
-public void setgradeCourseOne(double gradeCourseOne){
-    this.gradeCourseOne = gradeCourseOne;
-}
-
-public double getgradeCourseOne() {
-    return gradeCourseOne;
-}
-
- // Getters and Setters -> Student ID
-
-public void setStudentID(int studentID) {
-    this.studentID = studentID;
-}
-
-public int getStudentID(){
-    return studentID;
-}
-
-    // method Get Letter grade - Ranked grade A-F
-
-    public char getLetterGrade (){
-           char x = '#';
-        if (gradeCourseOne >=90) {
-            x = 'A';
-        } else if (gradeCourseOne >=80 && gradeCourseOne <90){
-            x = 'B';
-        } else if (gradeCourseOne >=70 && gradeCourseOne <80){
-            x = 'C';
-        } else if (gradeCourseOne >=60 && gradeCourseOne <70){
-            x = 'D';
-        } else if (gradeCourseOne >=50 && gradeCourseOne <60){
-            x = 'E';
-        } else if (gradeCourseOne <50){
-            x = 'F';
-        } else {
-            x = '#';
-        }
-        return x;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    // Method - Vowels counter
+    public String getFirstName(){
+        return firstName;
+    }
+
+        // Getters and Setters -> Last Name
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+        // Getters and Setters -> Grade Course One
+
+    public void setgradeCourseOne(double gradeCourseOne){
+        this.gradeCourseOne = gradeCourseOne;
+    }
+
+    public double getgradeCourseOne() {
+        return gradeCourseOne;
+    }
+
+        // Getters and Setters -> Student ID
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public int getStudentID(){
+        return studentID;
+    }
+
+        // Method Get Letter grade - Ranked grade A-F
+
+    public char getLetterGrade (){          
+           char gradeLetter = '#';                                              // Variable to save the grade letter 
+        if (gradeCourseOne >=90) {                                              // Condition to grade A
+            gradeLetter = 'A';
+        } else if (gradeCourseOne >=80 && gradeCourseOne <90){                  // Condition to grade B
+            gradeLetter = 'B';
+        } else if (gradeCourseOne >=70 && gradeCourseOne <80){                  // Condition to grade C
+            gradeLetter = 'C';
+        } else if (gradeCourseOne >=60 && gradeCourseOne <70){                  // Condition to grade D
+            gradeLetter = 'D';
+        } else if (gradeCourseOne >=50 && gradeCourseOne <60){                  // Condition to grade E
+            gradeLetter = 'E';
+        } else if (gradeCourseOne <50){                                         // Condition to grade F
+            gradeLetter = 'F';
+        } else {
+            gradeLetter = '#';                                                  // Condition to any other option (default error)
+        }
+        return gradeLetter;                                                     // Returning the grade letter.
+    }
+
+    // Method Get Vowel Count - First name's vowels counter
 
     public int getVowelCount(){
-        int counterVowels = 0;
-        firstName = firstName.toLowerCase();
+        int counterVowels = 0;                                                  // Set a counter to save the quantity of vowels                                  
+        firstName = firstName.toLowerCase();                                    // Converting input to lowercase (validity purpose)
 
-        for (int i = 0, n=firstName.length() ; i<n; i++){
-            if (firstName.charAt(i) == 'a' || firstName.charAt(i) == 'e' || firstName.charAt(i) == 'i' || firstName.charAt(i) == 'o' || firstName.charAt(i) == 'u') {
-                counterVowels++;
+        for (int i = 0, n=firstName.length() ; i<n; i++){                       // Iteraction to check if the letter ia a vowel
+            if (firstName.charAt(i) == 'a' || firstName.charAt(i) == 'e' || 
+                firstName.charAt(i) == 'i' || firstName.charAt(i) == 'o' ||     // Conditions to be a vowel or not
+                firstName.charAt(i) == 'u') {
+                    counterVowels++;                                            // Add 1 to each vowel found at iteraction
             }
         }
-    return counterVowels;
-}
+    return counterVowels;                                                       // Returning the quantity of vowels at first name.
+    }
 
 
 }
