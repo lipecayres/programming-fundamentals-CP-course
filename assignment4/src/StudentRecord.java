9d9b7a2d2c94f11839043fa803fd753e071b4a44
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-31 20:50:11 -0500

    method getLetterGrade added

--- assignment4/src/StudentRecord.java
+++ assignment4/src/StudentRecord.java
@@ -13,7 +13,7 @@ private String lastName;
 private double gradeCourseOne;
 private int studentID;
 
-    // declaring empty constructor
+    // Declaring empty constructor
     public StudentRecord  () {}
 
     // Constructor declaration 
@@ -24,40 +24,65 @@ private int studentID;
         this.studentID = studentID;
     }
 
-        // Setters
+ // Getters and Setters -> First Name
 
 public void setFirstName(String firstName) {
     this.firstName = firstName;
 }
 
-public void setLastName(String lastName) {
-    this.firstName = lastName;
-}
-
-public void setgradeCourseOne(double gradeCourseOne){
-    this.gradeCourseOne = gradeCourseOne;
-}
-
-public void setstudentID(int studentID) {
-    this.studentID = studentID;
-}
-
-        // Setters 
-
 public String getFirstName(){
     return firstName;
+}
+
+    // Getters and Setters -> Last Name
+
+public void setLastName(String lastName) {
+    this.firstName = lastName;
 }
 
 public String getLastName() {
     return lastName;
 }
 
+    // Getters and Setters -> Grade Course One
+
+public void setgradeCourseOne(double gradeCourseOne){
+    this.gradeCourseOne = gradeCourseOne;
+}
+
 public double getgradeCourseOne() {
     return gradeCourseOne;
+}
+
+ // Getters and Setters -> Student ID
+
+public void setstudentID(int studentID) {
+    this.studentID = studentID;
 }
 
 public int getstudentID(){
     return studentID;
 }
+
+    // method Get Letter grade - Ranked grade A-F
+
+    public char getLetterGrade (int gradeCourseOne){
+
+        if (gradeCourseOne >=90) {
+            return 'A';
+        } else if (gradeCourseOne >=80 && gradeCourseOne <90){
+            return 'B';
+        } else if (gradeCourseOne >=70 && gradeCourseOne <80){
+            return 'C';
+        } else if (gradeCourseOne >=60 && gradeCourseOne <70){
+            return 'D';
+        } else if (gradeCourseOne >=50 && gradeCourseOne <60){
+            return 'E';
+        } else if (gradeCourseOne <50){
+            return 'F';
+        } else {
+            return '#';
+        }
+    }
 
 }
