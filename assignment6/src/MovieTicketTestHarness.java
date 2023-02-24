aa94716f88dcb7727c3f0b62a7aef54be36bd275
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-24 01:03:38 -0500

    v1 code ok

--- assignment6/src/MovieTicketTestHarness.java
+++ assignment6/src/MovieTicketTestHarness.java
@@ -1,101 +1,67 @@
-/**	Application Purpose: Build the main method to give the JRE an entry point into the Airline program.  
+/**	Application Purpose: Build the main method to give the JRE an entry point into the Movie Ticket .  
 *	Author: Felipe Cayres
-*	Date: Feb 15th, 2023
-*	Time: 1:10 PM
+*	Date: Feb 23th, 2023
+*	Time: 11:34 PM
 */
 
 // Import statements:
 import java.util.Scanner;
     
-public class MovieTicketTestHarness {                                                    //Create a second class called AirlineReservation 
+public class MovieTicketTestHarness {                                                            //Create a second class called MovieTicketTestHarness 
     public static void main (String [] args) {                                                  // Main method
         
-                    //
-                    // Challenge 4
-                    //
-
-        //Instantiate a Airline Reservation object 
-        MovieTicket ar1 = new MovieTicket("Jack", "Blue", 1020, 3 , 334547);
-
-        //Print out object state.
-        System.out.println();                                                                   // Jumping line - better visualization
-        System.out.println("Airline Reservation Informations");                              // Print "example" title to user        
-        System.out.println("--- " + "First Name: " + ar1.getFirstName());                       // Print First Name     
-        System.out.println("--- " +"Last Name: " + ar1.getLastName());                          // Print Last Name
-        System.out.println("--- " +"Flight Number: " + ar1.getFlightNumber());                  // Print Flight Number
-        System.out.println("--- " +"Seat Number: " + ar1.getSeatNumber());                      // Print Seat Number
-        System.out.println("--- " +"Ticket Number: " + ar1.getTicketNumber());                  // Print Ticket Number
-        System.out.println("--- " +"Seating Class: " + ar1.retrieveSeatingClass());             // Print Seating Class
-        System.out.println("--- " +"Segment State: " + ar1.segmentState());                     // Print Segment State       
-        System.out.println("\n----------------------------");                                // Jumping line  and printing a bar to better visualization
-        System.out.println("--- " +"No Fly List: " + ar1.checkNoFlyList());   
+          // Instantiate ticket.
+        MovieTicket mt1 = new MovieTicket();   
         
-        if(ar1.checkNoFlyList().contains("9999")) {
-          System.out.println("ALERT ALERT ALERT ALERT ALERT ALERT ALERT ALERT ALERT");    
-        } else {
-          System.out.println("--- OK TO GO!");      
-        }
-
-                  // Print the checking No Fly List
-        System.out.println("\n----------------------------");  
-
-
-        // Instantiate a second Airline Reservation with no data.
-        MovieTicket ar2 = new MovieTicket();   
-        
-        // Ask the user for the data required to fill each field of the reservation
+        // Ask the user for the data required to fill each field of the ticket
         Scanner sc= new Scanner(System.in);                                                     // Set new Scanner to input data to system.  
         
-        System.out.print("Reservation: \n");                                                 // Display appropriate message to user
-        System.out.print("Type here the informations about the reservation:\n\n");
+        System.out.print("Movie Ticket's Information: \n");                                  // Display appropriate message to user
+        System.out.print("Insert here the requested informations:\n\n");
         
-                // Register first name
-        System.out.print("Enter First Name: ");                                               // Display appropriate messa to user
-        String firstName= sc.nextLine();                                                        // Input to First Name   
-        ar2.setFirstName(firstName);                                                            // Set method to save typed first name
+                // Register Movie name
+        System.out.print("Enter Movie Name: ");                                               // Display appropriate message to user
+        String movieName= sc.nextLine();                                                        // Input to Movie Name   
+        mt1.setMovieName(movieName);                                                            // Set method to save inserted data
 
-                // Register last name
-        System.out.print("Enter Last Name: ");                                                // Display appropriate messa to user
-        String lastName= sc.nextLine();                                                         // Input to last Name
-        ar2.setLastName(lastName);                                                              // Set method to save typed last name
 
-                // Register Flight Number
-        System.out.print("Enter Flight Number: ");                                           // Display appropriate messa to user
-        int flightNumber= sc.nextInt();                                                        // Input to Flight Number
-        ar2.setFlightNumber(flightNumber);                                                     // Set method to save typed Flight Number
+                // Register Ticket Number
+        System.out.print("Enter Ticket Number: ");                                           // Display appropriate messa to user
+        int ticketNumber= sc.nextInt();                                                        // Input to Ticket Number
+        mt1.setTicketNumber(ticketNumber);                                                     // Set method to save typed inserted data
                 
-                // Register Seat Number
-        System.out.print("Enter Seat Number: ");                                             // Display appropriate messa to user
-        int seatNumber= sc.nextInt();                                                          // Input to Seat Number
-        ar2.setSeatNumber(seatNumber);                                                         // Set method to save typed Seat Number
-                
-                // Register Flight Number
-        System.out.print("Enter Ticket Number: ");                                            // Display appropriate messa to user
-        int ticketNumber= sc.nextInt();                                                         // Input to grade course one
-        ar2.setTicketNumber(ticketNumber);                                                      // Set method to save typed Ticket Number
+                // Register Theatre Number
+        System.out.print("Enter Theatre Number: ");                                             // Display appropriate messa to user
+        int theatreNumber= sc.nextInt();                                                          // Input to Theatre Number
+        mt1.setTheatreNumber(theatreNumber);                                                      // Set method to save typed inserted data
 
+                // Display Movie Ticket's Informations
+        System.out.println("\n Movie Ticket's Informations");                                 // Display appropriate message to user       
+        System.out.println("--- " + "Movie Name: " + mt1.getMovieName());                       // Print Movie Name     
+        System.out.println("--- " +"Ticket Number: " + mt1.getTicketNumber());                  // Print Ticket Number
+        System.out.println("--- " +"Theatre Number: " + mt1.getTheatreNumber());                // Print Theatre Number
+ 
+                // Display Prices
+        System.out.println("\n --- " + "Price: $" + MovieTicket.ticketPrice);                           // Print Price before tax
+        System.out.printf("--- " + "Price after tax: $%.2f\n", MovieTicket.getPriceAfterTax());         // Print Price after tax
 
-        System.out.println();                                                                   // Jumping line - better visualization
-        System.out.println("Airline Reservation Informations");                              // Print "example" title to user        
-        System.out.println("--- " + "First Name: " + ar2.getFirstName());                       // Print First Name     
-        System.out.println("--- " +"Last Name: " + ar2.getLastName());                          // Print Last Name
-        System.out.println("--- " +"Flight Number: " + ar2.getFlightNumber());                  // Print Flight Number
-        System.out.println("--- " +"Seat Number: " + ar2.getSeatNumber());                      // Print Seat Number
-        System.out.println("--- " +"Ticket Number: " + ar2.getTicketNumber());                  // Print Ticket Number
-        System.out.println("--- " +"Seating Class: " + ar2.retrieveSeatingClass());             // Print Seating Class
-        System.out.println("--- " +"Segment State: " + ar2.segmentState());                     // Print Segment State       
-        System.out.println("\n----------------------------");                                // Jumping line  and printing a bar to better visualization
-        System.out.println("--- " +"No Fly List: " + ar2.checkNoFlyList());                     // Print No Fly check
-        
-                // Print the checking No Fly List
-        if(ar2.checkNoFlyList().contains("9999")) {                                          // If state to check No List
-          System.out.println("ALERT ALERT ALERT ALERT ALERT ALERT ALERT ALERT ALERT");       // Print Alert
-        } else {                                                                               // Else state to No Fly check
-          System.out.println("--- OK TO GO!");                                               // Print No Fly check
-        }
+                //BUG FIX
+        movieName= sc.nextLine();                                                                // BUX FIX - Jumping input line
+
+                // Display Available Dates | Movie
+        System.out.print("\n Enter Movie Name: ");                                                    // Display appropriate messa to user        
+        movieName= sc.nextLine();                                                                       // Input to Movie Number
+        System.out.println("\nAvailable Dates: \n --- " + mt1.inquireMovieRunDates(movieName));         // Display Available Dates
+
+                // Display Available Dates | Ticket                
+        System.out.print("\nEnter Ticket Number: ");                                                  // Display appropriate messa to user
+        ticketNumber= sc.nextInt();                                                                     // Input to Ticket Number
+        System.out.println("\nAvailable Dates: \n --- " + mt1.inquireMovieRunDates(ticketNumber));      // Display Available Dates
+
+                // Palindrome Check        
+        System.out.println("\n--- " +"Palindrom Check: " + mt1.checkPalindrom());                       // Print palindrome check
 
         System.out.println("\n----------------------------");                                // Jumping line - better visualization
-
 
         sc.close();                                                                             // Close Scanner
   }
