861d3016a84009c60588e76a1dfda3135336481c
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-03-15 08:54:57 -0400

    first commit

---
+++ assignment7/src/BankTransactionRecordTestHarness.java
@@ -0,0 +1,16 @@
+/**	Application Purpose: Build the main method to give the JRE an entry point into the Bank Transactions.  
+*	Author: Felipe Cayres
+*	Date: Mar 15th, 2023
+*	Time: 08:50 AM
+*/
+
+// Import statements:
+public class BankTransactionRecordTestHarness{
+    public static void main (String [] args) {
+          // Instantiate ticket.
+        BankTransaction mt1 = new BankTransaction();   
+
+
+    }
+}
+
