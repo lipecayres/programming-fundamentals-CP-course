37cbefedc0dd729919c9a88f25e0fa21285f58d7
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-03-15 10:53:39 -0400

    setting random informations to each piece of data

--- assignment7/src/BankTransactionRecordTestHarness.java
+++ assignment7/src/BankTransactionRecordTestHarness.java
@@ -3,12 +3,31 @@
 *	Date: Mar 15th, 2023
 *	Time: 08:50 AM
 */
+import java.util.Random;
 
 // Import statements:
 public class BankTransactionRecordTestHarness{
     public static void main (String [] args) {
+        Random random = new Random();
+
           // Instantiate Bank Accounts.
-        BankTransaction mt1 = new BankTransaction();   
+        BankTransaction[] account = new BankTransaction[500];   
+
+        System.out.println("\nBank Account Informations\n-----------------------");  
+
+        for (int i = 0; i<account.length;i++){
+               boolean canadianFunds = random.nextBoolean();
+               double exchangeRate = random.nextDouble();
+               int transactionNumber = random.nextInt();
+               long transactionReferenceNumber = random.nextLong();
+               
+                account[i] = new BankTransaction(canadianFunds, exchangeRate, transactionNumber, transactionReferenceNumber);
+
+                account[i].setCanadianFunds(canadianFunds);
+                account[i].setExchangeRate(exchangeRate);
+                account[i].setTransactionNumber(transactionNumber);
+                account[i].setTransactionReferenceNumber(transactionReferenceNumber);
+        }
 
 
     }
