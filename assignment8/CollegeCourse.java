ec5f0392b0bf7c6aa3b1a6590206f6f35f2aacad
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-03-21 14:49:55 -0400

    blueprint

---
+++ assignment8/CollegeCourse.java
@@ -0,0 +1,37 @@
+/**	Application Purpose: Build a blueprint College Course - states and behavior
+*	Author: Felipe Cayres
+*	Date: Mar 21st, 2023
+*	Time: 02:40 PM
+*/
+ 
+public class CollegeCourse {                       
+
+    // variables
+    private int courseReferenceNumber, courseNumber;
+
+    // declaring empty constructor
+    public CollegeCourse () {}
+
+    // constructor  with arguments 
+    public CollegeCourse (int courseReferenceNumber, int courseNumber) {
+        this.courseReferenceNumber = courseReferenceNumber;
+        this.courseNumber = courseNumber;
+    }
+
+        // Getters and Setters -> Course Reference Number
+    public void setCourseReferenceNumber(int courseReferenceNumber) {
+        this.courseReferenceNumber = courseReferenceNumber;
+    }
+    public int getCourseReferenceNumber(){
+        return courseReferenceNumber;
+    }
+
+        // Getters and Setters -> Course Number
+    public void setExchangeRate(int courseNumber){
+        this.courseNumber = courseNumber;
+    }
+    public int getExchangeRate() {
+        return courseNumber;
+    }
+
+}
