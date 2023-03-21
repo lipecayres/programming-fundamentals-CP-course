b050f48b8bc1559f4839aa6fe893cf1279e89e62
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-03-21 14:50:02 -0400

    first commit

---
+++ assignment8/CollegeCourseRecordTestHarness.java
@@ -0,0 +1,13 @@
+/**	Application Purpose: Build the main method to give the JRE an entry point into the Bank Transactions.  
+*	Author: Felipe Cayres
+*	Date: Mar 15th, 2023
+*	Time: 08:50 AM
+*/
+
+
+public class CollegeCourseRecordTestHarness{
+	public static void main (String [] args) {
+
+
+        }
+}
