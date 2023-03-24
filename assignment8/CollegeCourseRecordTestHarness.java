db27042db6d05899766766ade22610009ce46fcb
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-03-23 23:32:58 -0400

    level 3 ok

--- assignment8/CollegeCourseRecordTestHarness.java
+++ assignment8/CollegeCourseRecordTestHarness.java
@@ -4,18 +4,25 @@
 *	Time: 02:50 PM
 */
 
+import java.util.Random;
+import java.util.Scanner;
 
 public class CollegeCourseRecordTestHarness{
 	public static void main (String [] args) {
 
 		int row = 200, columns = 10, counter = 0; 
+		int courseCode[] = {11001,12002,13003,14004,15005};
+		
+		Random rand = new Random(); 
+		Scanner sc= new Scanner(System.in);
+
+		CollegeCourse cc;
 
 		CollegeCourse [][] collegeData = new CollegeCourse[row][columns];
 
 		for (int i = 0; i<row; i++){
 			for (int j = 0; j<columns;j++){
 				counter++; 
-				CollegeCourse cc;
 				
 				if (counter <= 1000){
 					cc = new CollegeCourse(0,0);
@@ -30,8 +37,45 @@ public class CollegeCourseRecordTestHarness{
 			System.out.println("Course Reference Number: " + collegeData[i][j].getCourseReferenceNumber());
 			System.out.println("Course Number: " + collegeData[i][j].getCourseNumber());
 			System.out.println("------------------------------");
-	
+
+
+			}
+		}
+
+		System.out.println("\n#### Level 2: ####\n");
+
+		// level 2
+		counter = 0;
+		for (int i = 0; i<row; i++){
+			for (int j = 0; j<columns;j++){
+				counter++;
+
+				int randomNumber = rand.nextInt(5); 
+
+				cc = new CollegeCourse(counter,courseCode[randomNumber]);
+
+				collegeData[i][j] = cc;
+
+			}
 
 		}
-	}
-}}
+
+		System.out.println("\n#### Level 3: ####\n");
+
+		// loop to print data
+		for (int i = 0; i<row; i++){
+			for (int j = 0; j<columns;j++){
+
+				String formatted = String.format("%07d", collegeData[i][j].getCourseReferenceNumber());
+
+				System.out.println("Course Reference Number: " + formatted);
+				System.out.println("Course Number: " + collegeData[i][j].getCourseNumber());
+				System.out.println("------------------------------");
+			}
+		}
+
+	}	
+
+}
+
+
