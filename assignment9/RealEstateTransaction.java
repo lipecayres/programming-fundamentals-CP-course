70ef7914fb86150c41c281562ec8930436bae4ef
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-04-03 09:20:28 -0400

    blueprint ok

--- assignment9/RealEstateTransaction.java
+++ assignment9/RealEstateTransaction.java
@@ -1,3 +1,4 @@
+
 /**
  * Application Purpose: Build a blueprint Real Estate Transaction - states and
  * behavior
@@ -6,10 +7,16 @@
  * Time: 07:55 PM
  */
 
+import java.util.Random;
+
 public class RealEstateTransaction {
 
     // variables
     private String streetNumber, streetName, city, purchasePrice;
+
+    Random rand = new Random();
+    String fullString = (streetNumber + streetName + city + purchasePrice).replaceAll(" ", "");
+    int fullStringLength = fullString.length();
 
     // default constructor
     public RealEstateTransaction() {
@@ -42,7 +49,7 @@ public class RealEstateTransaction {
         return streetName;
     }
 
-    // getters and setters -> street name
+    // getters and setters -> city
     public void setCity(String city) {
         this.city = city;
     }
@@ -51,13 +58,102 @@ public class RealEstateTransaction {
         return city;
     }
 
-    // getters and setters -> street name
+    // getters and setters -> purchase price
     public void setPurchasePrice(String purchasePrice) {
         this.purchasePrice = purchasePrice;
     }
 
     public String getPurchasePrice() {
         return purchasePrice;
+    }
+
+    // method 1 - generate state one string
+    public void generateStateOneString() {
+        
+        //get string data
+        String fullString = (streetNumber + streetName + city + purchasePrice).replaceAll(" ", "");
+        int fullStringLength = fullString.length();
+        
+        //split string in 2 parts
+        String part1 = "", part2 = "";
+        for (int i = 0; i < fullStringLength; i++) {
+
+            //append character using string length as paramether
+            if (i <= fullStringLength / 2) {
+                part1 += fullString.charAt(i);
+            } else if (i > fullStringLength / 2) {
+                part2 += fullString.charAt(i);
+            }
+        }
+        //print final state
+        System.out.println(part1.toLowerCase() + part2.toUpperCase());
+
+    }
+
+    // method 2 - generate random state one string
+    public void generateRandomStateOneString() {
+        //get string data
+        String fullString = (streetNumber + streetName + city + purchasePrice).replaceAll(" ", "");
+        int fullStringLength = fullString.length();
+
+        //set random value
+        Random rand = new Random();
+        int rand1 = 0;
+
+        while (rand1 <= 0) {
+            rand1 = rand.nextInt(fullStringLength - 1);
+        }
+
+        //split string in 2 parts
+        String part1 = "", part2 = "";
+
+        for (int i = 0; i < fullStringLength; i++) {
+
+            //append character using string length as paramether
+            if (i < rand1) {
+                part1 += fullString.charAt(i);
+            } else if (i >= rand1) {
+                part2 += fullString.charAt(i);
+            }
+        }
+        //print final state
+        System.out.println(part1.toLowerCase() + part2.toUpperCase());
+    }
+
+    // method 3 - encrypt string
+    public String encryptState(int inKeyCharacterIndex, int firstCharacterIndex) {
+        
+        //get string data
+        String fullString = (streetNumber + streetName + city + purchasePrice).replaceAll(" ", "");
+        int fullStringLength = fullString.length();
+
+        // error status - return 1
+        if (inKeyCharacterIndex >= fullString.length() || 
+            firstCharacterIndex >= fullString.length() || 
+            inKeyCharacterIndex >= firstCharacterIndex) {
+            return "1";
+        }
+
+        //encrypted string data
+        StringBuilder encryptedString = new StringBuilder();
+        char key = fullString.charAt(inKeyCharacterIndex);
+        
+        //if statement variable to replace char
+        float keyLock;
+
+        for (int i = 0; i<fullStringLength; i++){
+            keyLock = (i - firstCharacterIndex) % 3;
+
+            //append character using encrypted paramethers
+            if (i == firstCharacterIndex || 
+               (i > inKeyCharacterIndex && (keyLock== 0))){
+                encryptedString.append(key);
+               } else{
+                encryptedString.append(fullString.charAt(i));
+               }
+        }
+        // return final string
+        return encryptedString.toString();
     }
 
 }
